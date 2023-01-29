39b8dfbda47a1298c28e60a76004e0e4131b6fe0
sevalnuroncu <sevalnuroncu@gmail.com>
2023-01-29 18:46:00 +0300

    code reviewed and fixed

--- HouseTypesApplication/src/Service.java
+++ HouseTypesApplication/src/Service.java
@@ -4,75 +4,66 @@ import java.util.List;
 public class Service {
     ListingOperations listing=new ListingOperations();
 
+
     public int totalPriceHouse(){
-        int totalPriceHouse=0;
-        for(Building house:listing.getHouseList()){
-            totalPriceHouse+=house.getPrice();
-        }
-        return totalPriceHouse;
+        //return the total prices of houses
+        return totalPrice(listing.getHouseList());
     }
 
     public int totalPriceVilla(){
-        int totalPriceVilla=0;
-        for(Building villa:listing.getVillaList()){
-            totalPriceVilla+=villa.getPrice();
-        }
-        return totalPriceVilla;
+        //return the total prices of villas
+        return totalPrice(listing.getVillaList());
     }
 
     public int totalPriceSummerHouse(){
-        int totalPriceSummerHouse=0;
-        for(Building summerHouse:listing.getSummerHouseList()){
-            totalPriceSummerHouse+=summerHouse.getPrice();
-        }
-        return totalPriceSummerHouse;
+        //return the total prices of summer houses
+        return totalPrice(listing.getSummerHouseList());
     }
 
     public int totalPriceBuilding(){
-        int totalPriceBuilding=0;
-        totalPriceBuilding+=totalPriceHouse()+totalPriceVilla()+totalPriceSummerHouse();
-        return totalPriceBuilding;
+        //return the total prices of all types of houses
+        return totalPrice(listing.getAllBuilding());
     }
 
     public double averageOfHouseArea(){
-        double totalArea=0;
-        for(Building house:listing.getHouseList()){
-            totalArea+=house.getSquareMeters();
-        }
-        return totalArea/listing.getHouseList().size();
+        //return the average square meters of houses
+        return averageOfBuilding(listing.getHouseList());
     }
 
     public double averageOfVillaArea(){
-        double totalArea=0;
-        for(Building villa:listing.getVillaList()){
-            totalArea+=villa.getSquareMeters();
-        }
-        return totalArea/listing.getVillaList().size();
+        //return the average square meters of villas
+        return averageOfBuilding(listing.getVillaList());
     }
 
     public double averageOfSummerHouseArea(){
-        double totalArea=0;
-        for(Building summerHouse:listing.getSummerHouseList()){
-            totalArea+=summerHouse.getSquareMeters();
-        }
-        return totalArea/listing.getSummerHouseList().size();
+        //return the average square meters of summer houses
+        return averageOfBuilding(listing.getSummerHouseList());
     }
 
     public double averageOfAllBuilding(){
-        double total=0;
-        for(Building house:listing.getHouseList()){
-            total+=house.getSquareMeters();
-        }
-        for(Building villa:listing.getVillaList()){
-            total+=villa.getSquareMeters();
-        }
-        for(Building summerHouse:listing.getSummerHouseList()){
-            total+=summerHouse.getSquareMeters();
-        }
-
-        return total/(listing.getHouseList().size()+listing.getVillaList().size()+listing.getSummerHouseList().size());
+        //return the average square meters of all types of houses
+        return averageOfBuilding(listing.getAllBuilding());
     }
+    public double averageOfBuilding(List<Building> buildings){
+        //calculate total area according to entered parameter
+        double totalArea=0;
+        for(Building building:buildings){
+            totalArea+=building.getSquareMeters();
+        }
+        return totalArea/buildings.size();
+    }
+
+    public int totalPrice(List<Building> buildings){
+        //calculate total price according to entered parameter
+        int totalPrice=0;
+        for(Building building: buildings){
+            totalPrice+=building.getPrice();
+        }
+        return totalPrice;
+    }
+
     public List<Building> filteredCountOfRoomsAndLivingRooms(int numberOfRooms, int numberOfLivingRooms){
+        //filters and returns houses of all types according to the number of rooms and living rooms.
         List<Building> filteredBuilding=new ArrayList<>();
         for(int i=0; i<listing.getAllBuilding().size(); i++){
             if(listing.getAllBuilding().get(i).getNumberOfRooms()==numberOfRooms && listing.getAllBuilding().get(i).getNumberOfLivingRooms()==numberOfLivingRooms ){
